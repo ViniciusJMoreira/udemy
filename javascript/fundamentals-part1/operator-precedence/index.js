556f1c1ed97bc590fb6d425cf20c6d95b3ad1e98
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-04-04 22:21:17 +0200

    Include operators precedence

---
+++ javascript/fundamentals-part1/operator-precedence/index.js
@@ -0,0 +1,12 @@
+// Math operators
+const now = 2030;
+const ageJonas = now - 1991;
+const ageMaria = now - 2018;
+
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y); // x = 10, y = 10
+
+const averageAge = (ageJonas + ageMaria) / 2;
+console.log(averageAge)
