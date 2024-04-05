8977595dba6b1dc74c905ad33543321ac8ef900a
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-04-05 17:52:38 +0200

    Include template litorals

---
+++ javascript/fundamentals-part1/template-literals/index.js
@@ -0,0 +1,11 @@
+const firstName = 'Vinicius';
+const lastName = 'Moreira';
+const birthday = 1993;
+const nowYear = 2024;
+const job = 'Desenvolvedor'
+
+console.log(`
+Meu nome completo è ${firstName + ' ' + lastName}
+Tenho ${nowYear - birthday} anos
+E trabalho na area de ${job}
+`)
