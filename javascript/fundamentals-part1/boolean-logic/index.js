5a68d6dde0878ebfe5c25484466103990038c1c6
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-04-08 00:47:29 +0200

    Include boolean logic

---
+++ javascript/fundamentals-part1/boolean-logic/index.js
@@ -0,0 +1,15 @@
+// const age = 16;
+
+// if(age >= 18 && age < 30)console.log('age is greater than 18 and less than 30.');
+// if(!(age >= 18))console.log('age is not greater than 18');
+// if(age >= 18 || age < 30)console.log('age is greater than 18 or less than 30.');
+// if(!(age >= 18) && age < 30)console.log('age is not greater than 18 and less than 30.');
+// if(age >= 18 && !(age < 30))console.log('age is greater than 18 and is not less than 30.');
+
+// score koalas
+const scoreKoalas = (96+108+89) / 2;
+// score dolphins
+const scoreDplphins = (96+109+89) / 2;
+if(scoreDplphins > scoreKoalas)console.log('Dolphins win the trophy');
+else if(scoreDplphins < scoreKoalas)console.log('Koalas win the trophy');
+else console.log('Both win the trophy');
