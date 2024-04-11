31aac8a7ab2ff5a90d1b659e3ab54b59a30b88d1
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-04-11 10:47:49 +0200

    commit

--- javascript/fundamentals-part1/ternario/index.js
+++ javascript/fundamentals-part1/ternario/index.js
@@ -5,5 +5,5 @@ console.log(`I like to drink ${age >= 18 ? 'Wine 🍷' : 'Water 💧'}`);
 
 // calculator tips
 const bill = 275;
-const tip = bill >= 50 || bill <= 300 ? bill * (15/100) : bill * (20/100);
+const tip = bill >= 50 && bill <= 300 ? bill * (15/100) : bill * (20/100);
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`);
