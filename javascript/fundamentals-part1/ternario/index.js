38f01191d2c6d36d8419d2e9d890a6cea50f0a7b
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-04-11 10:41:35 +0200

    Update fundamentals part 2

--- javascript/fundamentals-part1/ternario/index.js
+++ javascript/fundamentals-part1/ternario/index.js
@@ -2,3 +2,8 @@ const age = 17;
 const drink = age >= 18 ? 'Wine 🍷' : 'Water 💧';
 console.log(`I like to drink ${drink}`);
 console.log(`I like to drink ${age >= 18 ? 'Wine 🍷' : 'Water 💧'}`);
+
+// calculator tips
+const bill = 275;
+const tip = bill >= 50 || bill <= 300 ? bill * (15/100) : bill * (20/100);
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`);
