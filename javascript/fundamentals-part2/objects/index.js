652ac39a5adede7f3e83689f8c6c7a1cf1513721
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-04-14 10:49:48 +0200

    include object introduction

---
+++ javascript/fundamentals-part2/objects/index.js
@@ -0,0 +1,15 @@
+const userArray = [
+  'Vinicius',
+  'Junqueira',
+  2024 - 1993,
+  'Developer',
+  ['Michael', 'Jonas', 'Jay']
+];
+
+const user = {
+  name: 'Vinicius',
+  lastName: 'Junqueira',
+  age: 2022 - 1993,
+  job: 'Developer',
+  friends: ['Michael', 'Jonas', 'Jay']
+};
