2f590dc7f22b8cf68044c86d574e4e5094fb813d
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-04-15 12:23:23 +0200

    Include loop backwars and , loop inside loop

---
+++ javascript/fundamentals-part2/looping-arrays/looping-backwards/index.js
@@ -0,0 +1,11 @@
+const john = [
+  'John',
+  'Miller',
+  2024 - 1993,
+  'Developer',
+  ['Jay', 'Marks', 'Michael']
+];
+
+for (let i = john.length - 1; i >= 0; i--) {
+  console.log(i, john[i]);
+}
---
+++ javascript/fundamentals-part2/looping-arrays/loops-in-loops/index.js
@@ -0,0 +1,6 @@
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-----exercise number ${exercise}`);
+  for(let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise} , lifting weight repetition ${rep}`);
+  }
+}
