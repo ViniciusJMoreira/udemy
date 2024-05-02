fb4bce695cc6bb43e17521da3e27ad674ca77ea0
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-05-02 16:17:04 +0200

    Include assignment operators

--- javascript/modernJs/assignmentOperators/index.js
+++ javascript/modernJs/assignmentOperators/index.js
@@ -1,29 +1,36 @@
+const date = new Date();
+const year = date.getFullYear();
+const month = ("0" + (date.getMonth() + 1)).slice(-2);;
+const day = ("0" + date.getDate()).slice(-2);
 const user1 = {
   fullname: {
-    name: 'Vinicius',
-    surname: 'Moreira',
+    name: "Vinicius",
+    surname: "Moreira",
+    nickname: "Vini"
   },
-  gener: 'Male',
-}
+  gender: "Male",
+  updateDate: "20/04/2024",
+};
 
 const user2 = {
   fullname: {
-    name: 'Andressa',
-    surname: 'Texeira',
+    name: "Andressa",
+    surname: "Texeira",
+    nickname: ""
   },
-  gener: 'Female',
-  phone: '+3391234567',
-}
+  gender: "Female",
+  phone: "+3391234567",
+};
 // (Or) assignment operator (null, undefined, NaN, 0, '', false)
-user1.phone ||= 'Indefinido';
-user2.phone ||= "Indefinido";
+user1.fullname.nickname ||= "Anonymous";
+user2.fullname.nickname ||= "Anonymous";
 
 // (And) assignment operator (null, undefined, NaN, 0, '', false)
-user1.phone &&= 'Indefinido';
-user2.phone &&= "Indefinido";
+user1.updateDate &&= `${day}/${month}/${year}`;
+user2.updateDate &&= `${day}/${month}/${year}`;
 
 // nullish assignment operator (null, undefined)
-user1.phone ??= 'Indefinido';
-user2.phone ??= "Indefinido";
+user1.phone ??= "Undefined";
+user2.phone ??= "Undefined";
 
-console.log(user1, user2);
+console.log( user1 , user2 );
