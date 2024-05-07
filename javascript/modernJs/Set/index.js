6f1d61935db794068a467d06b0e66cb2f7fc74e8
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-05-07 23:29:58 +0200

    update

---
+++ javascript/modernJs/Set/index.js
@@ -0,0 +1,64 @@
+const store = {
+  products: [
+    {
+      books: [
+        "Romance",
+        "Thriller",
+        "Dark Romance",
+        "Science Fiction",
+        "historical",
+        "Adventure",
+      ],
+      home: [
+        "Lighting",
+        "Fumiture & Decoration",
+        "Showroom",
+        "Cooking & Serving",
+        "Textiles & Mattresses",
+        "Creative Hobbies",
+        "Garden & Gardening",
+      ],
+      games: [
+        "Xbox One",
+        "Xbox Series X|S",
+        "Playstation 5",
+        "Playstation 4",
+        "PC Games",
+        "Virtual Reality",
+      ],
+      eletronics: [
+        "Telephony",
+        "Monitor",
+        "Laptop",
+        "Tablets",
+        "Desktop",
+        "Computer Components",
+        "Printers",
+        "Consoles",
+      ],
+    },
+  ],
+  categories: [
+    "books",
+    "home",
+    "games",
+    "eletronics",
+    // made on purpose
+    "games",
+    "home",
+  ],
+
+  getProduct() {
+    return this.products;
+  },
+};
+
+const set = new Set(store.categories);
+// console.log(set.size);
+// console.log(set.has('books'));
+// console.log(set.delete('games'));
+// console.log(set.add('games'));
+// console.log(set.add('sport'));
+// set.forEach(cat => console.log(cat));
+// set.clear();
+// console.log(set);
