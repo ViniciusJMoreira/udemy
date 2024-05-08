3f1ae137ec28c5a1983a737ad157aa9d3a991d16
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-05-08 15:44:07 +0200

    include object Map() and Set()+

--- javascript/modernJs/Map/index.js
+++ javascript/modernJs/Map/index.js
@@ -43,25 +43,34 @@ const store = {
     "home",
     "games",
     "eletronics",
-    // made on purpose
-    "games",
-    "home",
+    
   ],
-
   getProduct() {
     return this.products;
   },
 };
 
-const set = new Set(store.categories);
-// console.log(set.size);
-// console.log(set.has('books'));
-// console.log(set.delete('games'));
-// console.log(set.add('games'));
-// console.log(set.add('sport'));
-// set.forEach(cat => console.log(cat));
-// set.clear();
-// console.log(set);
-// const [...categories] = set;
-// store.categories = categories;
-// console.log(store);
+const map = new Map();
+// map.set('categories', ["books", "home", "games", "eletronics"]);
+// map.set('products', [{books:{}, home: {}, games: {}, eletronics: {}}]);
+// map.delete('categories');
+// console.log(map.get("products")); 
+for (const [, arr] of map) {
+  for (const value  of arr) {
+    const key = Object.keys(value);
+    const { books, home, games, eletronics } = value;
+    console.log(books, home, games, eletronics);
+    for (const item of key) {
+      console.log(item);
+    }
+  }
+}
+
+map.forEach(arr => {
+  arr.forEach(item => {
+    const {books, home, games, eletronics} = item;
+    const key = Object.keys(item);
+    console.log(books, home, games, eletronics);
+    key.forEach(value => console.log(value));
+  })
+})
