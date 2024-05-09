2eb7b2743436ec715148dab6e3abb0028470f125
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-05-09 18:24:20 +0200

    update

---
+++ javascript/modernJs/Map/script.js
@@ -0,0 +1,19 @@
+"use-strict";
+
+const map = new Map()
+  .set("question", "what is the most popular programming language ?")
+  .set(1, "Java")
+  .set(2, "PHP")
+  .set(3, "Javascript")
+  .set(4, "Python")
+  .set(true, "Correct🎉, the right answer is Javascript")
+  .set(false, "Wrong Choice😢");
+// console.log(map);
+const question = map.get('question');
+const asnwer = 'Javascript';
+console.log(map.get('question'));
+for (const [key,value] of map) {
+  if(typeof key !== 'number') continue;
+  console.log(`${key}: ${value}`);
+}
+console.log(map.get(map.get(3) === asnwer));
