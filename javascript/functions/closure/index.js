3be9c3d19f8150a6a34b3755438adb8ff5647a23
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-05-30 19:16:02 +0200

    Include more closure examples

--- javascript/functions/closure/index.js
+++ javascript/functions/closure/index.js
@@ -26,3 +26,16 @@ let internal;
 internal();
 
 console.dir(internal);
+
+////////////////////////
+
+const boardPassengers = function(n = 1, wait) {
+  const perGroup = n/3;
+  const time = setTimeout(() => {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start borading in ${wait} seconds.`);
+}
+
+boardPassengers(99,10);
