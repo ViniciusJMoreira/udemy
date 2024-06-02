b471ed8e0220fa7631e99fc31d05aeb50ebef77e
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-06-02 22:17:15 +0200

    include foreach array method and starting banking project

--- javascript/arrays/methods/index.js
+++ javascript/arrays/methods/index.js
@@ -2,26 +2,43 @@
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // SLICE
-console.log(arr.slice(0,1));
-console.log(arr.slice(2,3));
-console.log(arr.slice(2,4));
-console.log(arr.slice(3,5));
-console.log(arr.slice(2));
-console.log(arr.slice(-2));
+// console.log(arr.slice(0,1));
+// console.log(arr.slice(2,3));
+// console.log(arr.slice(2,4));
+// console.log(arr.slice(3,5));
+// console.log(arr.slice(2));
+// console.log(arr.slice(-2));
 
 // SPLICE
 arr.splice(-2);
-console.log(`Splice: ${arr}`);
+// console.log(`Splice: ${arr}`);
 arr = ["a", "b", "c", "d", "e"];
 
 // REVERSE
 const arr2 = ['j', 'i', 'h', 'g', 'f'].reverse();
-console.log(`Reverse: ${arr2}`);
+// console.log(`Reverse: ${arr2}`);
 
 // CONCAT
 const letters = arr.concat(arr2);
 // [...arr, ...arr2];
-console.log(`Concat: ${letters}`);
+// console.log(`Concat: ${letters}`);
 
 // JOIN
-console.log(`Join: ${letters.join('-')}`);
+// console.log(`Join: ${letters.join('-')}`);
+
+// AT
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
+// console.log(arr.at(0));
+// console.log(arr.at(2));
+
+//FOREACH
+for (const [i,letter] of Object.entries(letters)) {
+  // console.log(`Letter n.${i} = ${letter}`);
+}
+letters.forEach((letter,i,arr) => {
+  // console.log(`Letter n.${i} = ${letter}`);
+  // console.log(arr);
+})
+
--- javascript/arrays/script.js
+++ javascript/arrays/script.js
@@ -74,3 +74,20 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+function displayMovements(movement) {
+  containerMovements.innerHTML = '';
+  movement.forEach((mov,i) => {
+    const type = mov < 0 ? 'withdrawal' : 'deposit';
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i} ${type}</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  })
+}
+
+displayMovements(movements);
