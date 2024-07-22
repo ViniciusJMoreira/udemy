ac7f42e9cb8cbc9e25e4ab75dc66fc82fa48fb48
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-07-22 20:22:38 +0200

    Include manipulation dom advanced

--- javascript/advancedDom/index.js
+++ javascript/advancedDom/index.js
@@ -1,16 +1,23 @@
 const header = document.querySelector(".header");
+const headerNav = document.querySelector(".nav");
+const navList = document.querySelector(".nav__list");
+const firstLink = document.querySelector(".nav__link:first-child");
 const imgLogo = document.querySelector('.nav__logo');
+
+// criando elementos no dom e adicionando atributos
 const cookie = document.createElement("div");
 cookie.classList = 'cookie-message';
 cookie.textContent = "We use cookied for improved functionality and analytics.";
 const cookieBtn = document.createElement("button");
 cookieBtn.classList = "btn btn--close-cookie";
 cookieBtn.textContent = "Got it!";
+// adiionando elemento filho no elemento pai
 cookie.appendChild(cookieBtn);
 // header.append(cookie);
 // header.prepend(cookie);
 // header.after(cookie);
-header.before(cookie);
+// header.before(cookie);
+// evento eliminando elemento
 cookieBtn.addEventListener("click", () => {
   cookie.remove();
 })
@@ -23,14 +30,64 @@ document.documentElement.style.cssText = "background-color: blue, color: #fff";
 // console.log(getComputedStyle(document.documentElement));
 
 // visualizar, modificar e criar atributos
-console.log(imgLogo.getAttribute("alt"));
+// console.log(imgLogo.getAttribute("alt"));
 // modificar atributos
 imgLogo.setAttribute("alt", "Amazing Bankist Logo");
 // criar atributos
 imgLogo.setAttribute("title", "Bankist Logo");
-console.log(imgLogo.getAttribute("alt"));
-console.log(imgLogo.getAttribute("title"));
+// console.log(imgLogo.getAttribute("alt"));
+// console.log(imgLogo.getAttribute("title"));
 // imgLogo.dataset.imageTarget = "new data";
 imgLogo.setAttribute("data-image-target", "new data");
-console.log(imgLogo.dataset.imageTarget);
+// console.log(imgLogo.dataset.imageTarget)
 
+// window.addEventListener("scroll", () => {
+//   const windowCoords = section1.getBoundingClientRect();
+//   document.querySelector('.misuras').textContent = window.scrollY;
+// })
+
+// funcao para gerar um numero aleatorio
+function randonColor() {
+  return Math.floor(Math.random() * 255);
+}
+// Efeito borbulhamento no event listener , afetando tambem o elemento pai quando dispara o evento no elemento target.
+firstLink.addEventListener("click", function(e) {
+  this.style.backgroundColor = `rgb(${randonColor()},${randonColor()},${randonColor()})`;
+  console.log(e.target);
+  console.log(e.currentTarget);
+  e.stopPropagation();
+},true);
+// para disparar os eventos dos elementos pai , é preciso que seja do mesmo evento(click)
+navList.addEventListener("click", function(e) {
+  this.style.backgroundColor = `rgb(${randonColor()},${randonColor()},${randonColor()})`;
+  console.log(e.target);
+  console.log(e.currentTarget);
+});
+headerNav.addEventListener("click", function(e) {
+  this.style.backgroundColor = `rgb(${randonColor()},${randonColor()},${randonColor()})`;
+  console.log(e.target);
+  console.log(e.currentTarget);
+});
+
+const h1 = document.querySelector("h1");
+
+h1.addEventListener("click", function() {
+  //////////////////////////
+  //children
+  // childNodes retorna um array de tags textos comentarios, tudo o que tiver dentro do elemento pai
+  console.log(this.childNodes);
+  // children retorna um array de todas as tags dentro do elemento pai
+  console.log(this.children);
+  //////////////////////////
+  //parent
+  // retorna o elemento pai
+  console.log(this.parentNode);
+  console.log(this.parentElement);
+  //////////////////////////
+  //sibling
+  console.log(this.previousElementSibling);
+  console.log(this.nextElementSibling);
+
+  // seleciona todos elementos pai do elemento selecionado
+  console.log(h1.closest('h1'));
+})
--- javascript/advancedDom/script.js
+++ javascript/advancedDom/script.js
@@ -1,13 +1,19 @@
 'use strict';
-
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav__list");
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
 
+
+///////////////////////////////////////
+// Modal window
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -28,3 +34,55 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////////
+// Header Window
+navMenu.addEventListener("click", function(e) {
+  e.preventDefault();
+  if(e.target.classList.contains("nav__link") && !e.target.classList.contains("nav__link--btn")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({behavior: "smooth"});
+  }
+})
+
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  // console.log(s1coords);
+  // console.log(header.getBoundingClientRect());
+  // console.log(e.target.getBoundingClientRect());
+
+  // window.scrollTo(s1coords.left + window.scrollX, s1coords.top + window.scrollY);
+
+  window.scrollTo({
+    top: s1coords.top + window.scrollY,
+    left: s1coords.left + window.scrollX,
+    behavior: 'smooth',
+  });
+
+  // section1.scrollIntoView({behavior: 'smooth'});
+});
+
+///////////////////////////////////////
+// Operations section
+tabContainer.addEventListener("click", function(event) {
+  const currentTab = event.target.closest('.operations__tab');
+
+  // guard clause
+  if(!currentTab) return;
+
+  // remove actve class
+  document.querySelector(".operations__tab--active")
+  .classList.remove("operations__tab--active");
+  document.querySelector(".operations__content--active")
+  .classList.remove("operations__content--active");
+
+  // active tab
+  currentTab.classList.add('operations__tab--active');
+
+  // active content tab
+  document.querySelector(`.operations__content--${currentTab.dataset.tab}`)
+  .classList.add("operations__content--active");
+});
+
